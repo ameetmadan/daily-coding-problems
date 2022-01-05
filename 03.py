c44d968d80128f2168fde102ca28769889e21898
ameetmadan <ameetmadan@gmail.com>
2022-01-05 21:19:00 +0100

    4 & 5 added, 3 WIP

--- 03.py
+++ 03.py
@@ -1,4 +1,5 @@
-# Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+# Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s),
+# which deserializes the string back into the tree.
 
 # For example, given the following Node class
 
@@ -12,3 +13,24 @@
 # node = Node('root', Node('left', Node('left.left')), Node('right'))
 # assert deserialize(serialize(node)).left.left.val == 'left.left'
 
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    def deserialize(self, s):
+        if s == '#':
+            return None
+        else:
+            node = Node(s)
+            node.left = self.deserialize(s[1])
+            node.right = self.deserialize(s[2])
+            return node
+    def serialize(self, node):
+        if node is None:
+            return '#'
+        else:
+            return str(node.val) + self.serialize(node.left) + self.serialize(node.right)
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert node.deserialize(node.serialize(node)).left.left.val == 'left.left'
--- 04.py
+++ 04.py
@@ -1,5 +1,30 @@
 # Good morning! Here's your coding interview problem for today.
 # This problem was asked by Stripe.
-# Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+# Given an array of integers, find the first missing positive integer in linear time and constant space. 
+# In other words, find the lowest positive integer that does not exist in the array. The array can contain 
+# duplicates and negative numbers as well.
 # For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
 # You can modify the input array in-place.
+
+def find_missing_positive_integer(arr):
+    # The idea is to use the array as a hash table and iterate through the array
+    # to mark the position of the integer in the hash table.
+    # If the integer is negative, ignore it.
+    # If the integer is positive, mark it in the hash table.
+    # If the integer is not in the hash table, return the integer.
+    # If the integer is in the hash table, ignore it.
+    # If the array is empty, return 1.
+    if len(arr) == 0:
+        return 1
+    hash_table = {}
+    for i in range(len(arr)):
+        if arr[i] > 0:
+            hash_table[arr[i]] = True
+    for i in range(1, len(arr) + 2):
+        if i not in hash_table:
+            return i
+    return len(arr) + 1
+
+# print(find_missing_positive_integer([3, 4, -1, 1]))
+assert(find_missing_positive_integer([3, 4, -1, 1]) == 2)
+assert(find_missing_positive_integer([1, 2 ,0]) == 3)
