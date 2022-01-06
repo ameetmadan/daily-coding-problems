ede8dd2b41251a529a1e28e3edce47e299cd9d6d
ameetmadan <ameetmadan@gmail.com>
2022-01-06 19:03:27 +0100

    7 and 9 added

--- jan/07.py
+++ jan/07.py
@@ -7,3 +7,47 @@
 # For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
 
 # You can assume that the messages are decodable. For example, '001' is not allowed.
+
+def decode(s):
+    if len(s) == 0:
+        return 0
+    if len(s) == 1:
+        return 1
+    if len(s) == 2:
+        if int(s) <= 26:
+            return 2
+        else:
+            return 1
+    if int(s[:2]) <= 26:
+        return decode(s[1:]) + decode(s[2:])
+    else:
+        return decode(s[1:])
+
+print(decode('111'))
+assert(decode('111') == 3)
+print(decode('12'))
+print(decode('123'))
+print(decode('1234'))
+print(decode('12345'))
+print(decode('123456'))
+print(decode('1234567'))
+print(decode('12345678'))
+print(decode('123456789'))
+print(decode('1234567890'))
+print(decode('12345678901'))
+print(decode('123456789012'))
+print(decode('1234567890123'))
+print(decode('12345678901234'))
+print(decode('123456789012345'))
+print(decode('1234567890123456'))
+print(decode('12345678901234567'))
+print(decode('123456789012345678'))
+print(decode('1234567890123456789'))
+print(decode('12345678901234567890'))
+print(decode('123456789012345678901'))
+print(decode('1234567890123456789012'))
+print(decode('12345678901234567890123'))
+print(decode('123456789012345678901234'))
+print(decode('1234567890123456789012345'))
+print(decode('12345678901234567890123456'))
+print(decode('123456789012345678901234567'))
--- jan/09.py
+++ jan/09.py
@@ -7,3 +7,20 @@
 # For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
 
 # Follow-up: Can you do this in O(N) time and constant space?
+
+def largest_sum(numbers):
+    if len(numbers) == 0:
+        return 0
+    if len(numbers) == 1:
+        return numbers[0]
+    if len(numbers) == 2:
+        return max(numbers[0], numbers[1])
+    if numbers[0] > numbers[1]:
+        return largest_sum(numbers[1:]) + numbers[0]
+    else:
+        return largest_sum(numbers[1:]) + numbers[1]
+
+assert(largest_sum([2, 4, 6, 2, 5]) == 13)
+print(largest_sum([2, 4, 6, 2, 5]))
+assert(largest_sum([5, 1, 1, 5]) == 10)
+print(largest_sum([5, 1, 1, 5]))
