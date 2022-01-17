3a5cc06f3557a0e07aed8d61150b88e8429e3767
ameetmadan <ameetmadan@gmail.com>
2022-01-17 21:47:44 +0100

    added more challenges

--- jan/13.py
+++ jan/13.py
@@ -5,3 +5,13 @@
 # Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
 
 # For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+def longest_substring(k, s):
+    longest = 0
+    for i in range(len(s)):
+        for j in range(i+1, len(s)+1):
+            if len(set(s[i:j])) <= k:
+                longest = max(longest, len(s[i:j]))
+    return longest
+
+print(longest_substring(2, "abcba"))
